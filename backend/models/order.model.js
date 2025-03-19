562583e5f0641f5e7f48313ee53256c3d663be2b
yulirealman <yulirealman@gmail.com>
2025-03-19 14:00:18 -0500

    add new Model for order

---
+++ backend/models/order.model.js
@@ -0,0 +1,67 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema(
+  {
+    room_number: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 17,
+    },
+    phone_number: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    arrival_date: {
+      type: Date,
+      required: true,
+    },
+    checkout_date: {
+      type: Date,
+      required: true,
+    },
+    base_rate: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    gst: {
+      type: Number,
+      required: false,
+      min: 0,
+    },
+    pst: {
+      type: Number,
+      required: false,
+      min: 0,
+    },
+    accommodation_tax: {
+      type: Number,
+      required: false,
+      min: 0,
+    },
+    deposit: {
+      type: Number,
+      required: false,
+      min: 0,
+    },
+    payment_method: {
+      type: String,
+      enum: ['cash', 'visa', 'mc', 'debitcard'],
+      required: true,
+    },
+    notes: {
+      type: String,
+      default: '',
+    },
+  },
+  { timestamps: true }
+);
+
+const Order = mongoose.model("Order", orderSchema);
+
+export default Order;
